d4247ce44ee3bde475ffd462b753c22cd3dd6623
chithams <chithams@student.ethz.ch>
2016-05-25 19:44:00 +0200

    --

--- xdmvcserver.js
+++ xdmvcserver.js
@@ -170,6 +170,7 @@ XDmvcServer.prototype.startPeerSever = function(port){
             }
         }
         that.deletePeerJsPeer(deviceId);
+        
         that.emit("disconnected", deviceId);
     });
 
@@ -422,7 +423,9 @@ XDmvcServer.prototype.handleAjaxRequest = function(req, res, next){
             res.end();
             break;
         case 'getFriendsByGroup':
+            console.log(this.userDevices);
             if(this.userIds[query.id]){
+
                 var userID = this.userIds[query.id];
                 if(this.friendsByGroup[userID]){
                     if(query.data == "all"){
@@ -461,9 +464,9 @@ XDmvcServer.prototype.handleAjaxRequest = function(req, res, next){
         case 'enterRelationship':
             console.log("enterRelationship");
             if(query.data){
-                var userID = query.data[0];
-                var contactID = query.data[1];
-                var relationshipName = query.data[2];
+                var userID = this.userIds[query.id];
+                var contactID = query.data[0];
+                var relationshipName = query.data[1];
                 if(this.relationships[userID]){
                     this.relationships[userID][contactID] = relationshipName;
                 }
@@ -609,7 +612,9 @@ XDmvcServer.prototype.handleAjaxRequest = function(req, res, next){
               //  delete this.peers[query.id].users ;
                 delete this.dict[userID];
                 delete this.deviceLocations[query.id];
-                delete this.userDevices[userID][query.id];
+                if(this.userDevices[userID]){
+                    delete this.userDevices[userID][query.id];
+                }
                 delete this.userIds[query.id];
                 if(Object.keys(this.userDevices[userID]).length > 0){
                     //not last device for this user
@@ -626,6 +631,7 @@ XDmvcServer.prototype.handleAjaxRequest = function(req, res, next){
         case 'userSignIn':
             //var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
             if(this.peers[query.id]){
+                console.log("signing In");
                 var split_list = query.data.split('.');
                 var jose_header = split_list[0];
                 var payload = split_list[1];
@@ -650,6 +656,10 @@ XDmvcServer.prototype.handleAjaxRequest = function(req, res, next){
                 }
                 console.log(this.userDevices);
 
+            }
+            else{
+                console.log("no entry in this.peers")
+                console.log(this.peers);
             }
             res.end();
             break;
